a2c0c28b52bd97d63af24fe29e13af1f30e9dcfe
Niketh Rajeev <nandunikethrajeev@gmail.com>
2026-04-24 18:07:06 +0530

    customized vite.conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(new URL('.', import.meta.url).pathname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-    build: {
-    outDir: 'dist',  // Output directory for the build
-    sourcemap: true,  // Enable source maps for production debugging
-    minify: 'terser',  // Use terser for minification
-    brotliSize: false, // Disable Brotli size report,
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].[hash].js',  // Naming convention for entry chunks
-        chunkFileNames: '[name].[hash].js',  // Naming convention for chunk files
-        assetFileNames: '[name].[hash][extname]', // Naming convention for asset files
-      }
-    }
-  },
-  server: {
-    port: 3000,      // Local development server port
-    open: true,      // Open the browser on server start
-    cors: true,      // Enable CORS
   },
 });
